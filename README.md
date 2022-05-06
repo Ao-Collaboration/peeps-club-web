db90024190ea661685f46422c9fbe7f8a1f2f192
Michael Standen <michael@aocollab.tech>
2022-05-06 15:20:53 +1200

    PC-51 Clone base from ooo-marketplace-web

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# peeps-club-web
-Wallet connected peeps club content
+# Peeps Club Web
+
+The front end website for wallet connected Peeps Club functionality
+
+## Usage
+
+Install dependencies:
+
+[NodeJS](https://nodejs.org/en/download)
+[Yarn](https://classic.yarnpkg.com/lang/en/docs/install)
+
+```sh
+yarn
+```
+
+Start the app:
+
+```sh
+yarn start
+```
---
+++ src/utils/doFetch.tsx
@@ -0,0 +1,16 @@
+const doFetch = async (path: string, method: string, body?: any) => {
+	const res = await fetch(path, {
+		method: method,
+		body: body && JSON.stringify(body),
+		credentials: 'include',
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		}
+	})
+	const data = await res.json()
+
+	return data
+}
+
+export default doFetch
