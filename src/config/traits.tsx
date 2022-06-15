dcfbe0310debdf7c89ccd61d86778f4989893e8c
Jono Shields <jonathonshields@gmail.com>
2022-06-15 16:53:13 +1200

    updated landingLocations and added defaultPeep

--- src/config/traits.tsx
+++ src/config/traits.tsx
@@ -1,13 +1,21 @@
+import { Appearance } from '../interface/metadata'
+
 export const landingLocations = [{
 	location: 'Japan',
-	bg: 'White'
+	district: 'Hills'
 },
 {
 	location: 'Jamaica',
-	bg: 'Yellow'
+	district: 'Desert'
 },
-{
-	location: 'New Zealand',
-	bg: 'Green'
-}
 ]
+
+export const defaultPeep: Appearance = {
+	skinTone: 'Limestone',
+	eyeColour: 'Dark Brown',
+	eyeStyle: 'Bow',
+	hairColor: 'Grey',
+	hairStyle: 'Messy Bun',
+	facialHair: 'None',
+	skinConditions: 'None'
+}
