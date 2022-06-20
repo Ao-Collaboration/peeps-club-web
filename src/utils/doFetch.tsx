1d4468fc5074e105c8ffde17d43b5a02c89f263b
Jono Shields <jonathonshields@gmail.com>
2022-06-21 09:14:14 +1200

    Add svg type for fetch

--- src/utils/doFetch.tsx
+++ src/utils/doFetch.tsx
@@ -1,15 +1,22 @@
+type Mimetypes  = 'image/svg+xml'
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const doFetch = async (path: string, method: string, body?: any) => {
+const doFetch = async (path: string, method: string, body?: any, mimetype?:Mimetypes) => {
 	const res = await fetch(path, {
 		method: method,
 		body: body && JSON.stringify(body),
 		credentials: 'omit',
 		headers: {
 			'Content-Type': method !== 'GET' ? 'application/json' : 'text/plain',
-			'Accept': 'application/json'
+			'Accept': mimetype || 'application/json'
 		}
 	})
-	const data = await res.json()
+	let data
+	if(mimetype === 'image/svg+xml'){
+		data = await res.text()
+	} else {
+		data = await res.json()
+	}
 
 	return data
 }
