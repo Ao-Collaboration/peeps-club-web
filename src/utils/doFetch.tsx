af4a925d275527a9644c63234a231400a3cd5e0d
Jono Shields <jonathonshields@gmail.com>
2022-06-22 14:13:07 +1200

    Fix for cors and svg calls

--- src/utils/doFetch.tsx
+++ src/utils/doFetch.tsx
@@ -2,18 +2,21 @@ type Mimetypes  = 'image/svg+xml'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const doFetch = async (path: string, method: string, body?: any, mimetype?:Mimetypes) => {
-	const res = await fetch(path, {
+	const options : RequestInit = {
+		mode: 'cors',
 		method: method,
 		body: body && JSON.stringify(body),
-		credentials: 'omit',
 		headers: {
-			'Content-Type': method !== 'GET' ? 'application/json' : 'text/plain',
-			'Accept': mimetype || 'application/json'
+			'Accept': mimetype || 'application/json',
 		}
-	})
+	}
+	if(method !== 'GET'){
+		options.headers = {...options.headers, 'Content-Type': 'application/json'}
+	}
+	const res = await fetch(path, options)
 	let data
 	if(mimetype === 'image/svg+xml'){
-		data = await res.text()
+		data = await res.blob()
 	} else {
 		data = await res.json()
 	}
