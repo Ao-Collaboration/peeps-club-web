225a16a298a90361557c89a5b2b5322be800e0b7
Michael Standen <michael@aocollab.tech>
2022-10-01 21:58:00 +1300

    Fix minutes in time remaining calc

--- src/utils/format.tsx
+++ src/utils/format.tsx
@@ -2,8 +2,8 @@ export const formatRemainingTime = (endTimestamp: number) => {
 	const left = endTimestamp - Math.floor(Date.now() / 1000)
 	let hours = Math.floor(left / 60 / 60)
 	const days = Math.floor(hours / 24)
-	hours -= days * 24
 	const minutes = Math.floor((left - hours * 60 * 60) / 60)
+	hours -= days * 24
 
 	if (days > 0) {
 		return `${days}d ${hours}h ${minutes}m`
