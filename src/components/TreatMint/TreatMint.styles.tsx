f638419d02eb148e5caedb4106370371c0df6fd9
Michael Standen <michael@aocollab.tech>
2022-10-22 22:05:11 +1300

    PC-188 Hand goes down

--- src/components/TreatMint/TreatMint.styles.tsx
+++ src/components/TreatMint/TreatMint.styles.tsx
@@ -1,6 +1,21 @@
 import { createUseStyles } from 'react-jss'
 
 const styles = {
+	arm: {
+		height: '80vh',
+		position: 'absolute',
+		margin: 'auto',
+		bottom: '70vh',
+		transition: 'bottom 3s ease',
+	},
+	armDown: {
+		bottom: '20vh',
+	},
+	bag: {
+		position: 'absolute',
+		margin: 'auto',
+		bottom: '10vh',
+	},
 }
 
 export default createUseStyles(styles)
--- src/components/TreatMint/TreatMint.tsx
+++ src/components/TreatMint/TreatMint.tsx
@@ -7,37 +7,38 @@ import useStyles from './TreatMint.styles'
 
 interface Props {
 	treatContract: Contract
-	peepId: BigNumberish|null
-	price: BigNumber|null
+	peepId: BigNumberish | null
+	price: BigNumber | null
 }
 
-const MintPublic: React.FC<Props> = ({
-	treatContract,
-	peepId,
-	price,
-}) => {
+const MintPublic: React.FC<Props> = ({ treatContract, peepId, price }) => {
 	const classes = useStyles()
+	const [isDown, setIsDown] = useState(false)
 	const [isLoading, setIsLoading] = useState(false)
 	const [pendingHash, setPendingHash] = useState<string | null>(null)
 
 	const mint = async () => {
-		setIsLoading(true)
-
-		try {
-			const tx = await (peepId ? treatContract.mintPeeps(peepId) : treatContract.mintPublic({ value: price }))
-			setPendingHash(tx.hash)
-			await tx.wait()
-			setPendingHash(null)
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		} catch (err: any) {
-			if (err?.error?.code === -32000) {
-				toast.error('You don\'t have enough ETH!')
-			} else {
-				toast.error('Unable to mint!')
+		setIsDown(true)
+		setTimeout(async () => {
+			try {
+				const tx = await (peepId
+					? treatContract.mintPeeps(peepId)
+					: treatContract.mintPublic({ value: price }))
+				setIsLoading(true)
+				setPendingHash(tx.hash)
+				await tx.wait()
+				setPendingHash(null)
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			} catch (err: any) {
+				if (err?.error?.code === -32000) {
+					toast.error('You don\'t have enough ETH!')
+				} else {
+					toast.error('Unable to mint!')
+				}
+			} finally {
+				setIsLoading(false)
 			}
-		} finally {
-			setIsLoading(false)
-		}
+		}, 3000)
 	}
 
 	return (
@@ -45,14 +46,23 @@ const MintPublic: React.FC<Props> = ({
 			{isLoading ? (
 				<Loading hash={pendingHash} />
 			) : (
-				<div>
-					<Button
-						onClick={mint}
-						className='primary'
-					>
-						Take some Treats!
-					</Button>
-				</div>
+				<>
+					{!isDown && (
+						<Button onClick={mint} className='primary'>
+							Take some Treats!
+						</Button>)
+					}
+					<img
+						aria-hidden
+						className={`${classes.arm} ${ isDown && classes.armDown}`}
+						src={'/assets/claw.svg'}
+					/>
+					{/* <img
+						aria-hidden
+						className={`${classes.bag}`}
+						src={'/assets/stamp_arm.svg'}
+					/> */}
+				</>
 			)}
 		</>
 	)
